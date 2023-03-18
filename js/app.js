fa596fc3ed82ab1ec3ce7cb7324c887e8a8011b3
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-17 22:45:35 -0300

    programmed auto hide for other accordions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const openAccord = (e) => {
-  e.target.parentNode.querySelector('p').classList.toggle('hide');
-  e.target.querySelector('img').classList.toggle('flip');
-  e.target.classList.toggle('btn-active');
+  document.querySelectorAll(".accordion").forEach((el) => {
+    if(el != e.target.parentNode) {
+      el.querySelector("button").classList.remove("btn-active");
+      el.querySelector("button").querySelector("img").classList.remove("flip");
+      el.querySelector("p").classList.add("hide");
+    }
+  });
+
+  e.target.parentNode.querySelector("p").classList.toggle("hide");
+  e.target.querySelector("img").classList.toggle("flip");
+  e.target.classList.toggle("btn-active");
 };
 
-document.querySelectorAll("button")
-  .forEach(btn => btn.addEventListener("click", openAccord));
+document
+  .querySelectorAll("button")
+  .forEach((btn) => btn.addEventListener("click", openAccord));
